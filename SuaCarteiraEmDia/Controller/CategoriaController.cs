ba270b431765d1a814a2a65a39ef5dd11f5e9236
Pedro Antônio Justino de Almeida <77810931+PedroAJAlmeida@users.noreply.github.com>
2024-07-05 13:14:44 -0300

    update--

---
+++ SuaCarteiraEmDia/Controller/CategoriaController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SuaCarteiraEmDia.Data;
+using SuaCarteiraEmDia.Model;
+
+namespace SuaCarteiraEmDia.Controller
+{
+    public class CategoriaController
+    {
+        public static void CriarCategoria(Categoria categoria)
+        {
+            // validar entradas 
+            if (categoria.Nome.Trim() == "")
+            {
+                Console.WriteLine("Digite um nome para a categoria");
+                return;
+            }
+
+
+            if (categoria.Cor.Trim() == "")
+            {
+                Console.WriteLine("Digite uma cor para a categoria");
+                return;
+            }
+
+            using (DataContext db = new DataContext())
+            {
+                if (db.Categorias.Any(x => x.Nome == categoria.Nome))
+                {
+                    Console.WriteLine("Título já existe");
+                    return;
+                }
+
+                categoria.UsuarioID = 1;
+                categoria.DataCricao = DateTime.Now;
+                categoria.DataAlteracao = DateTime.Now;
+                categoria.Ativo = true;
+                db.Categorias.Add(categoria);
+                
+                db.SaveChanges();
+
+
+
+
+            }
+        }
+
+
+    }
+}
+
+  
---
+++ SuaCarteiraEmDia/View/Catagoria/FormCategoria.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace SuaCarteiraEmDia.View.Catagoria
+{
+    partial class FormCategoria
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            button1 = new Button();
+            colorDialog1 = new ColorDialog();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(106, 85);
+            label1.Name = "label1";
+            label1.Size = new Size(40, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Nome";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(106, 112);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(172, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            button1.Location = new Point(292, 181);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 3;
+            button1.Text = "Escolher cor";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // textBox2
+            // 
+            textBox2.Enabled = false;
+            textBox2.Location = new Point(112, 182);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(20, 23);
+            textBox2.TabIndex = 4;
+            // 
+            // textBox3
+            // 
+            textBox3.Enabled = false;
+            textBox3.Location = new Point(138, 182);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(80, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(120, 266);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 6;
+            button2.Text = "Adicionar";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // FormCategoria
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(button1);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Name = "FormCategoria";
+            Text = "FormCategoria";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+        private Label label1;
+        private TextBox textBox1;
+        private Button button1;
+        private ColorDialog colorDialog1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button2;
+    }
+}
