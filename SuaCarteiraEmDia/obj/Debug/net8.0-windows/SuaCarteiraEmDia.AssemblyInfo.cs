2046ef71cb0f747febebb5643ae80bd7a30b64bc
Pedro Antônio Justino de Almeida <77810931+PedroAJAlmeida@users.noreply.github.com>
2024-07-09 12:51:50 -0300

    new string connection DB

--- SuaCarteiraEmDia/obj/Debug/net8.0-windows/SuaCarteiraEmDia.AssemblyInfo.cs
+++ SuaCarteiraEmDia/obj/Debug/net8.0-windows/SuaCarteiraEmDia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuaCarteiraEmDia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+41dba105e3697110293748af76c96c51f43188b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuaCarteiraEmDia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuaCarteiraEmDia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
