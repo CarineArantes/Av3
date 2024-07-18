a0486fee2f142b6041041a3bb3f47142d48fe2bc
Pedro Antônio Justino de Almeida <77810931+PedroAJAlmeida@users.noreply.github.com>
2024-07-18 19:30:32 -0300

    update

--- SuaCarteiraEmDia/obj/Debug/net8.0-windows/SuaCarteiraEmDia.AssemblyInfo.cs
+++ SuaCarteiraEmDia/obj/Debug/net8.0-windows/SuaCarteiraEmDia.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SuaCarteiraEmDia")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+09818c9fde3b60fd2c5cd780db5641b5d5b160dd")]
 [assembly: System.Reflection.AssemblyProductAttribute("SuaCarteiraEmDia")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SuaCarteiraEmDia")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
