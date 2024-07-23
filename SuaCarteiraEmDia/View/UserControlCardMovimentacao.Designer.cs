8fbe91aa404e9fd12a2ac5a523fbf2c9fbd20d31
Pedro Antônio Justino de Almeida <77810931+PedroAJAlmeida@users.noreply.github.com>
2024-07-23 17:57:20 -0300

    up

--- SuaCarteiraEmDia/View/UserControlCardMovimentacao.Designer.cs
+++ SuaCarteiraEmDia/View/UserControlCardMovimentacao.Designer.cs
@@ -28,97 +28,142 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(UserControlCardMovimentacao));
             tableLayoutPanel1 = new TableLayoutPanel();
-            textBox5 = new TextBox();
-            textBox4 = new TextBox();
-            textBox3 = new TextBox();
-            textBox2 = new TextBox();
-            textBox1 = new TextBox();
+            tableLayoutPanel2 = new TableLayoutPanel();
+            textBoxValor = new TextBox();
+            textBoxCategoria = new TextBox();
+            panel1 = new Panel();
+            pictureBox = new PictureBox();
+            richTextBoxDescricao = new RichTextBox();
             tableLayoutPanel1.SuspendLayout();
+            tableLayoutPanel2.SuspendLayout();
+            panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)pictureBox).BeginInit();
             SuspendLayout();
             // 
             // tableLayoutPanel1
             // 
-            tableLayoutPanel1.ColumnCount = 5;
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 20F));
-            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 20F));
-            tableLayoutPanel1.Controls.Add(textBox5, 4, 0);
-            tableLayoutPanel1.Controls.Add(textBox4, 3, 0);
-            tableLayoutPanel1.Controls.Add(textBox3, 2, 0);
-            tableLayoutPanel1.Controls.Add(textBox2, 1, 0);
-            tableLayoutPanel1.Controls.Add(textBox1, 0, 0);
+            tableLayoutPanel1.ColumnCount = 1;
+            tableLayoutPanel1.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 26.1194038F));
+            tableLayoutPanel1.Controls.Add(tableLayoutPanel2, 0, 0);
             tableLayoutPanel1.Dock = DockStyle.Fill;
+            tableLayoutPanel1.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
             tableLayoutPanel1.Location = new Point(0, 0);
             tableLayoutPanel1.Name = "tableLayoutPanel1";
             tableLayoutPanel1.RowCount = 1;
-            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 100F));
-            tableLayoutPanel1.Size = new Size(627, 30);
+            tableLayoutPanel1.RowStyles.Add(new RowStyle(SizeType.Percent, 53.8461533F));
+            tableLayoutPanel1.Size = new Size(936, 40);
             tableLayoutPanel1.TabIndex = 0;
             // 
-            // textBox5
+            // tableLayoutPanel2
             // 
-            textBox5.Dock = DockStyle.Fill;
-            textBox5.Location = new Point(503, 3);
-            textBox5.Name = "textBox5";
-            textBox5.Size = new Size(121, 23);
-            textBox5.TabIndex = 4;
+            tableLayoutPanel2.BackColor = Color.WhiteSmoke;
+            tableLayoutPanel2.ColumnCount = 5;
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 22.0512829F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Percent, 77.9487152F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 216F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 281F));
+            tableLayoutPanel2.ColumnStyles.Add(new ColumnStyle(SizeType.Absolute, 114F));
+            tableLayoutPanel2.Controls.Add(textBoxValor, 1, 0);
+            tableLayoutPanel2.Controls.Add(textBoxCategoria, 2, 0);
+            tableLayoutPanel2.Controls.Add(panel1, 0, 0);
+            tableLayoutPanel2.Controls.Add(richTextBoxDescricao, 3, 0);
+            tableLayoutPanel2.Dock = DockStyle.Fill;
+            tableLayoutPanel2.Location = new Point(0, 0);
+            tableLayoutPanel2.Margin = new Padding(0);
+            tableLayoutPanel2.Name = "tableLayoutPanel2";
+            tableLayoutPanel2.RowCount = 1;
+            tableLayoutPanel2.RowStyles.Add(new RowStyle(SizeType.Percent, 50F));
+            tableLayoutPanel2.Size = new Size(936, 40);
+            tableLayoutPanel2.TabIndex = 0;
             // 
-            // textBox4
+            // textBoxValor
             // 
-            textBox4.Dock = DockStyle.Fill;
-            textBox4.Location = new Point(378, 3);
-            textBox4.Name = "textBox4";
-            textBox4.Size = new Size(119, 23);
-            textBox4.TabIndex = 3;
+            textBoxValor.BackColor = Color.WhiteSmoke;
+            textBoxValor.BorderStyle = BorderStyle.None;
+            textBoxValor.Dock = DockStyle.Fill;
+            textBoxValor.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBoxValor.ForeColor = Color.Black;
+            textBoxValor.Location = new Point(81, 10);
+            textBoxValor.Margin = new Padding(10);
+            textBoxValor.Name = "textBoxValor";
+            textBoxValor.Size = new Size(233, 18);
+            textBoxValor.TabIndex = 1;
+            textBoxValor.TextAlign = HorizontalAlignment.Center;
             // 
-            // textBox3
+            // textBoxCategoria
             // 
-            textBox3.Dock = DockStyle.Fill;
-            textBox3.Location = new Point(253, 3);
-            textBox3.Name = "textBox3";
-            textBox3.Size = new Size(119, 23);
-            textBox3.TabIndex = 2;
+            textBoxCategoria.BackColor = Color.WhiteSmoke;
+            textBoxCategoria.BorderStyle = BorderStyle.None;
+            textBoxCategoria.Dock = DockStyle.Fill;
+            textBoxCategoria.Font = new Font("Segoe UI", 9.75F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            textBoxCategoria.ForeColor = Color.Black;
+            textBoxCategoria.Location = new Point(334, 10);
+            textBoxCategoria.Margin = new Padding(10);
+            textBoxCategoria.Name = "textBoxCategoria";
+            textBoxCategoria.Size = new Size(196, 18);
+            textBoxCategoria.TabIndex = 2;
+            textBoxCategoria.Text = "Oii";
+            textBoxCategoria.TextAlign = HorizontalAlignment.Center;
             // 
-            // textBox2
+            // panel1
             // 
-            textBox2.Dock = DockStyle.Fill;
-            textBox2.Location = new Point(128, 3);
-            textBox2.Name = "textBox2";
-            textBox2.Size = new Size(119, 23);
-            textBox2.TabIndex = 1;
+            panel1.Controls.Add(pictureBox);
+            panel1.Location = new Point(0, 0);
+            panel1.Margin = new Padding(0);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(71, 40);
+            panel1.TabIndex = 3;
             // 
-            // textBox1
+            // pictureBox
             // 
-            textBox1.Dock = DockStyle.Fill;
-            textBox1.Location = new Point(3, 3);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(119, 23);
-            textBox1.TabIndex = 0;
+            pictureBox.BackColor = Color.Black;
+            pictureBox.Dock = DockStyle.Fill;
+            pictureBox.Image = (Image)resources.GetObject("pictureBox.Image");
+            pictureBox.Location = new Point(0, 0);
+            pictureBox.Name = "pictureBox";
+            pictureBox.Size = new Size(71, 40);
+            pictureBox.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox.TabIndex = 4;
+            pictureBox.TabStop = false;
+            // 
+            // richTextBoxDescricao
+            // 
+            richTextBoxDescricao.BackColor = Color.WhiteSmoke;
+            richTextBoxDescricao.BorderStyle = BorderStyle.None;
+            richTextBoxDescricao.Dock = DockStyle.Fill;
+            richTextBoxDescricao.Location = new Point(543, 3);
+            richTextBoxDescricao.Name = "richTextBoxDescricao";
+            richTextBoxDescricao.Size = new Size(275, 34);
+            richTextBoxDescricao.TabIndex = 4;
+            richTextBoxDescricao.Text = "";
             // 
             // UserControlCardMovimentacao
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
+            BorderStyle = BorderStyle.FixedSingle;
             Controls.Add(tableLayoutPanel1);
             Name = "UserControlCardMovimentacao";
-            Size = new Size(627, 30);
+            Size = new Size(936, 40);
             Load += UserControlCardMovimentacao_Load;
             tableLayoutPanel1.ResumeLayout(false);
-            tableLayoutPanel1.PerformLayout();
+            tableLayoutPanel2.ResumeLayout(false);
+            tableLayoutPanel2.PerformLayout();
+            panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)pictureBox).EndInit();
             ResumeLayout(false);
         }
 
         #endregion
 
         private TableLayoutPanel tableLayoutPanel1;
-        private TextBox textBox5;
-        private TextBox textBox4;
-        private TextBox textBox3;
-        private TextBox textBox2;
-        private TextBox textBox1;
+        private TableLayoutPanel tableLayoutPanel2;
+        private TextBox textBoxValor;
+        private TextBox textBoxCategoria;
+        private Panel panel1;
+        private PictureBox pictureBox;
+        private RichTextBox richTextBoxDescricao;
     }
 }
--- SuaCarteiraEmDia/View/UserControlCardMovimentacao.cs
+++ SuaCarteiraEmDia/View/UserControlCardMovimentacao.cs
@@ -22,11 +22,29 @@ namespace SuaCarteiraEmDia.View.Principal
 
         private void UserControlCardMovimentacao_Load(object sender, EventArgs e)
         {
-            textBox1.Text = Movimentacao.Tipo.ToString();
-            textBox2.Text = Movimentacao.Valor.ToString();
-            textBox3.Text = Movimentacao.Categoria.Nome.ToString();
-            textBox4.Text = Movimentacao.Descricao.ToString();
-            textBox5.Text = Movimentacao.DataMovimentacao.Date.ToString();
+            Image img = pictureBox.Image;
+            textBoxValor.Text = Movimentacao.Valor.ToString();
+            textBoxCategoria.Text = Movimentacao.Categoria.Nome.ToString();
+            richTextBoxDescricao.Text = Movimentacao.Descricao.ToString();
+            textBoxCategoria.ForeColor = ColorTranslator.FromHtml(Movimentacao.Categoria.Cor);
+
+            // Centraliza o texto no RichTextBox
+            richTextBoxDescricao.SelectAll(); // Seleciona todo o texto
+            richTextBoxDescricao.SelectionAlignment = HorizontalAlignment.Center; // Alinha o texto ao centro
+            richTextBoxDescricao.DeselectAll(); // Deseleciona o texto
+
+            if (Movimentacao.Tipo == 'S')
+            {
+                img.RotateFlip(RotateFlipType.Rotate180FlipNone);
+                pictureBox.BackColor = Color.FromArgb(222, 73, 56);
+                textBoxValor.ForeColor = Color.FromArgb(222, 73, 56);
+            }
+            else
+            {
+                pictureBox.BackColor = Color.FromArgb(0, 167, 89);
+                textBoxValor.ForeColor = Color.FromArgb(0, 167, 89);
+            }
+            pictureBox.Image = img;
         }
     }
 }
